32db48249ddf8147614140f2836b0d39f7c00dd2
Matthieu Keller <keller.mdpa@gmail.com>
2014-10-26 19:54:00 +0100

    change date format, change article url, change theme, add license and source code repository, try to add items in menu

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,6 +9,7 @@ SITEURL = '/blog'
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
+DEFAULT_DATE_FORMAT='%d %b %Y'
 
 DEFAULT_LANG = 'en'
 
@@ -16,6 +17,10 @@ DEFAULT_LANG = 'en'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
+
+# Article
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
@@ -28,13 +33,28 @@ SOCIAL = (('Github', 'https://github.com/maggick'),
         ('Linked In', 'https://linkedin.com/in/matthieukeller')
         )
 
+# Menu
+MENUITEMS = (
+  ('Portal', 'www.matthieukeller.com'),
+  ('Blog', '/'),
+  ('Notes', 'www.matthieukeller.com/notes'),
+)
+
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = "/home/maggick/Documents/pelican-themes/bootstrap"
+THEME = "/home/maggick/Documents/pelican-themes/maggner-pelican/"
 
 # Following items are often useful when publishing
 DISQUS_SITENAME = "matthieukeller"
 #GOOGLE_ANALYTICS = ""
+
+# License
+LICENSE = 'Creative Commons Attribution 4.0'
+LICENSE_URL = 'http://creativecommons.org/licenses/by-sa/4.0/'
+LICENSE_TITLE = 'Share, adapt, use. But mention the author and keep the same license'
+SOURCE_CODE_URL = 'https://github.com/maggick/blog/'
+SOURCE_CODE_REPOSITORY = 'GitHub'
+
