3785a2a8c24df4215d2366562b16accb250df28f
Matthieu Keller <keller.mdpa@gmail.com>
2015-02-10 23:10:40 +0100

    put all conf in one file

--- publishconf.py
+++ publishconf.py
@@ -7,17 +7,66 @@ from __future__ import unicode_literals
 
 import os
 import sys
-from pelicanconf import *
 sys.path.append(os.curdir)
 
 SITEURL = 'http://www.matthieukeller.com/blog/'
 # RELATIVE_URLS = False
 
 FEED_DOMAIN = SITEURL
-FEED_ATOM = 'rss'
+FEED_ATOM = 'feeds/all.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+# DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 DISQUS_SITENAME = "matthieukeller"
 # GOOGLE_ANALYTICS = ""
+
+AUTHOR = 'Matthieu Keller'
+SITENAME = "maggick's logs"
+SITEURL = '/blog'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+DEFAULT_DATE_FORMAT = '%d %b %Y'
+
+DEFAULT_LANG = 'en'
+
+# Article
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/maggick'),
+          ('Twitter', 'https://twitter.com/matthieukeller'),
+          ('Linked In', 'https://linkedin.com/in/matthieukeller')
+          )
+
+# Menu
+MENUITEMS = (
+  ('Portal', 'www.matthieukeller.com'),
+  ('Blog', '/'),
+  ('Notes', 'www.matthieukeller.com/notes'),
+)
+
+DEFAULT_PAGINATION = 10
+
+THEME = "/home/maggick/work/pelican-themes/maggner-pelican/"
+STATIC_PATHS = ['images']
+
+# Following items are often useful when publishing
+DISQUS_SITENAME = "matthieukeller"
+# GOOGLE_ANALYTICS = ""
+
+# License
+LICENSE = 'Creative Commons Attribution 4.0'
+LICENSE_URL = 'http://creativecommons.org/licenses/by-sa/4.0/'
+LICENSE_TITLE = 'Share, adapt, use.\
+        But mention the author and keep the same license'
+SOURCE_CODE_URL = 'https://github.com/maggick/blog/'
+SOURCE_CODE_REPOSITORY = 'GitHub'
