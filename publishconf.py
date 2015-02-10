7eb8a4e16536e27ae2b2f238f6adb8ed2436c552
Matthieu Keller <keller.mdpa@gmail.com>
2015-02-10 22:46:30 +0100

    Try to fix url errors and add rss feed

--- publishconf.py
+++ publishconf.py
@@ -7,17 +7,17 @@ from __future__ import unicode_literals
 
 import os
 import sys
-sys.path.append(os.curdir)
 from pelicanconf import *
+sys.path.append(os.curdir)
 
 SITEURL = 'http://www.matthieukeller.com/blog/'
-RELATIVE_URLS = False
+# RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_DOMAIN = SITEURL
+FEED_ATOM = 'rss'
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 DISQUS_SITENAME = "matthieukeller"
-#GOOGLE_ANALYTICS = ""
+# GOOGLE_ANALYTICS = ""
