118e6622e073335ef9271ff8d90adcaa4705504a
maggick <keller.mdpa@gmail.com>
2019-09-29 09:54:12 +0200

    remove better_code_samples as it breaks everything

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,7 +30,7 @@ DISPLAY_CATEGORIES_ON_MENU = True
 
 # Plugins
 PLUGIN_PATHS = ['./pelican-plugins/']
-PLUGINS = ['summary', 'tag_cloud', 'extract_toc', 'better_code_samples']
+PLUGINS = ['summary', 'tag_cloud', 'extract_toc']#, 'better_code_samples']
 #MARKDOWN = {'extensions':['markdown.extensions.codehilite(css_class=highlight)', 'markdown.extensions.extra', 'markdown.extensions.toc']}
 
 # Social
