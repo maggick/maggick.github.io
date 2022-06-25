a834165f684b521fc8e54eed75a9bbd5f52903a6
maggick <maggick@users.noreply.github.com>
2022-06-25 15:22:28 +0200

    no sure what's going wrong

--- pelicanconf.py
+++ pelicanconf.py
@@ -64,15 +64,14 @@ CC_LICENSE = {
     "slug": "by"
 }
 
-def border(image):
-    """Add a border to an image"""
-    return image
+#def border(image):
+    #"""Add a border to an image"""
     #return ImageOps.expand(Image.open(image),border=300,fill='black').convert()
 
 IMAGE_PROCESS_FORCE = True
 IMAGE_PROCESS = {
     "article-image": {
         "type": "image",
-        "ops": [border],
+        "ops": [],
     },
 }
